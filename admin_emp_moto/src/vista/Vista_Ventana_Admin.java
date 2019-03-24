58b3a6a85005bae4b79ff51808013fca720b65c9
FuryMercury <furymercury18@gmail.com>
2019-03-24 16:03:25 -0500

    zzzz

--- admin_emp_moto/src/vista/Vista_Ventana_Admin.java
+++ admin_emp_moto/src/vista/Vista_Ventana_Admin.java
@@ -2,13 +2,20 @@
 package vista;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
+import javax.swing.JScrollPane;
 
 public class Vista_Ventana_Admin extends javax.swing.JFrame {
 
+    private Vista_Panel_Menu menu;
+    
     public Vista_Ventana_Admin() {
         initComponents();
+        menu =new Vista_Panel_Menu();
+        administrarPanel(jsMenuBar,menu);
         configFrame();
+        
     }
     
     private void configFrame(){    
@@ -19,28 +26,59 @@ public class Vista_Ventana_Admin extends javax.swing.JFrame {
         setResizable(false);
         setVisible(true);
     }
+    
+    public void administrarPanel(JScrollPane contenedor,Component compt){
+        contenedor.setViewportView(compt);
+    }
 
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
+
+        jsUserBar = new javax.swing.JScrollPane();
+        jsMenuBar = new javax.swing.JScrollPane();
+        jsContent = new javax.swing.JScrollPane();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        getContentPane().setLayout(new java.awt.GridBagLayout());
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipadx = 1280;
+        gridBagConstraints.ipady = 100;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 1.0;
+        getContentPane().add(jsUserBar, gridBagConstraints);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.ipadx = 50;
+        gridBagConstraints.ipady = 620;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 1.0;
+        getContentPane().add(jsMenuBar, gridBagConstraints);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipadx = 1230;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 1.0;
+        getContentPane().add(jsContent, gridBagConstraints);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JScrollPane jsContent;
+    public javax.swing.JScrollPane jsMenuBar;
+    public javax.swing.JScrollPane jsUserBar;
     // End of variables declaration//GEN-END:variables
 }
