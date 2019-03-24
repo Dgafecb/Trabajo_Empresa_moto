0204bb855eaf6be181f62a4f2fddbb22679e62c1
FuryMercury <furymercury18@gmail.com>
2019-03-24 00:51:47 -0500

    Vista_admin

--- admin_emp_moto/src/admin_emp_moto/Admin_emp_moto.java
+++ admin_emp_moto/src/admin_emp_moto/Admin_emp_moto.java
@@ -1,24 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package admin_emp_moto;
 
 import controlador.Controlador_log;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
 import modelo.Modelo_log;
 import vista.Vista_log;
 
-/**
- *
- * @author Usuario
- */
 public class Admin_emp_moto {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
+        
+        try{
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JDialog.setDefaultLookAndFeelDecorated(true);
+        UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+        //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+        //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        //UIManager.setLookAndFeel(ch.randelshofer.quaqua.QuaquaManager.getLookAndFeel());
+        }catch (Exception e){
+        e.printStackTrace();
+        }
+        
         Modelo_log mod = new Modelo_log();
         Vista_log view = new Vista_log();
         Controlador_log ctrl = new Controlador_log(view,mod);
